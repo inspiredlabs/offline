81be2c69603b820bf4f0e4ed503c38dabf419260
Scott Phillips <alan.scott.phillips@protonmail.ch>
2022-05-03 13:40:07 +0200

    Draggable mods.

---
+++ src/lib/Component.svelte
@@ -0,0 +1,47 @@
+<!------------------------------------------------------------------------------
+--  for additional, more detailled examples visit                             --
+--  https://github.com/rozek/svelte-agnostic-draggable/blob/main/README.md    --
+------------------------------------------------------------------------------->
+
+<style>
+  .draggable {
+		display:block;
+		position:absolute;
+		left:20px;
+		top:20px;
+		width:100px;
+		text-align:center;
+		padding:10px;
+		background:forestgreen;
+		color:white;
+		cursor:grab;
+
+    -webkit-touch-callout:none;
+    -ms-touch-action:none; touch-action:none;
+    -moz-user-select:none; -webkit-user-select:none; -ms-user-select:none; user-select:none;
+  }
+
+</style>
+
+<script>
+  import { draggable } from 'svelte-agnostic-draggable'
+
+/**** map all touch events to mouse events ****/
+
+  import mapTouchToMouseFor from 'svelte-touch-to-mouse'
+  mapTouchToMouseFor('.draggable')
+
+/**** Svelte Event Handling
+function onDraggableInit ()    { console.log('Draggable was created') }
+function onDragStart ()        { console.log('dragging started') }
+function onDragMove ()         { console.log('dragging continues') }
+function onDragStop ()         { console.log('dragging was stopped') }
+function onDraggableDestroy () { console.log('Draggable was destroyed') }
+****/
+</script>
+
+  <aside class="draggable absolute z-max w5 pa2" use:draggable={{
+    containment:'parent', cursor:'grabbing'
+  }}>Drag me!</aside>
+<!-- `<div on:draggable:init={onDraggableInit} on:draggable:destroy={onDraggableDestroy}
+    on:drag:start={onDragStart} on:drag:move={onDragMove} on:drag:stop={onDragStop} />` -->
---
+++ src/lib/Test.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let MyComponent;
+
+  onMount(async () => {
+      const module = await import('$lib/InspectorGadget.svelte');
+      MyComponent = module.default;
+  });
+</script>
+
+<svelte:component this={MyComponent} />
